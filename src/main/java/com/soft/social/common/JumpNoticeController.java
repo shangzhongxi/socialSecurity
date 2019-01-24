45e9a7f428cbe150450dc89690d32aa97111b448
Newhms <1023978473@qq.com>
2019-01-24 20:11:45 +0800

    将用户须知显示的API从swagger中去掉

--- src/main/java/com/soft/social/common/JumpNoticeController.java
+++ src/main/java/com/soft/social/common/JumpNoticeController.java
@@ -2,9 +2,11 @@ package com.soft.social.common;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Controller
 public class JumpNoticeController {
+    @ApiIgnore
     @RequestMapping("/notice")
     public String hello() {
 
